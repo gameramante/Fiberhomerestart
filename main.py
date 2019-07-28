25baeb483a56199c997f5379c3e63d35c80a1d82
mante <gamercaipira@gmail.com>
2019-07-28 20:57:18 -0300

    Restart router created

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'http://192.168.1.1/goform/webLogin'
+
+
+login_data = {
+    "User": "xxxx",
+    "Passwd": "xxxx"
+}
+ses = requests.Session()
+
+login = ses.post(url, data = login_data)
+reboot = ses.get('http://192.168.1.1/goform/reboot')
