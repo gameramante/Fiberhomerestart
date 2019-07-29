d75153c929535198aa3fab9616c46dacae2868b1
mante <gamercaipira@gmail.com>
2019-07-29 10:07:39 -0300

    import sys, add sys.exit()

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
 import time
+import sys
 
 url = 'http://192.168.1.1/goform/webLogin'
 
 def reboot():
     
     login_data = {
-        "User": "xxxxx",
-        "Passwd": "xxxxx"
+        "User": "xxxx",
+        "Passwd": "xxxx"
     }
 
     ses = requests.Session()
@@ -16,12 +17,14 @@ def reboot():
         ses.post(url, data = login_data)
     except requests.exceptions.ConnectionError:
         print('The router is still restarting')
-        time.sleep(5)
+        time.sleep(3)
+        sys.exit(0) #exit the program
     
     try:
         ses.get('http://192.168.1.1/goform/reboot')
     except requests.exceptions.ConnectionError:
         print('The router is now restarting.')
         time.sleep(5)
+        sys.exit(0)
 
 reboot()
