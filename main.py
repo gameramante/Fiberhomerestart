d7c88651a4b8597817a9f4a0c3b2514c26a94e3e
mante <gamercaipira@gmail.com>
2019-07-30 16:10:28 -0300

    moved code from main.py to restart.py. main.py now will only be responsible to check if my ONU IP is in the iprange.txt, if it is not then then main.py calls restart.py.

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import requests
-import time
 import sys
+import time
+from bs4 import BeautifulSoup
+from restart import reboot
 
 url = 'http://192.168.1.1/goform/webLogin'
-
-def reboot():
-    
-    login_data = {
-        "User": "xxxxx",
+login_data = {
+        "User": "xxxx",
         "Passwd": "xxxxx"
     }
 
-    ses = requests.Session()
+ses = requests.Session()
 
-    try:
-        ses.post(url, data = login_data)
-    except requests.exceptions.ConnectionError:
-        print('The router is still restarting')
-        time.sleep(3)
-        sys.exit(0) #exit the program
-    
-    try:
-        ses.get('http://192.168.1.1/goform/reboot')
-    except requests.exceptions.ConnectionError:
-        print('The router is now restarting.')
-        time.sleep(5)
-        sys.exit(0)
+try:
+    ses.post(url, data = login_data)
+except requests.exceptions.ConnectionError:
+    print('The router is still restarting')
+    time.sleep(3)
+    sys.exit(1) #exit the program
 
-reboot()
+checkip = ses.get('http://192.168.1.1/state/wan_state.asp').content
+soup = BeautifulSoup(checkip, 'html.parser')
+ip = soup.find(id = 'wan_ip').text
+print('Current ip {}'.format(ip))
+
+of = open('ip/iprange.txt', encoding = 'utf-8').read()
+if ip in of:
+    print('The ip was found in the database')
+else:
+    print('ip not found in database')
+    print('Restarting')
+    reboot()
