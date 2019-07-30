543d1b8a219bc4a38f2e0c89745af19d448c9ba8
mante <gamercaipira@gmail.com>
2019-07-30 18:42:56 -0300

    fixed bugs, added delay

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import sys
 import time
+import os
 from bs4 import BeautifulSoup
 
 login_data = {
-            "User": "xxxx",
-            "Passwd": "xxxx"
+            "User": "xxx",
+            "Passwd": "xxx"
         }
 
 url = 'http://192.168.1.1/goform/webLogin'
@@ -14,23 +15,28 @@ def main():
     ses = requests.Session()
 
     try:
-        ses.post(url, data = login_data)
+        print('Trying to connect to ONU...')
+        time.sleep(2)
+        ses.post(url, data = login_data) #login into the router
     except requests.exceptions.ConnectionError:
-        print('The router is still restarting.\n')
-        print('Trying again in 10 seconds.\n')
+        print('\n\nONU is still restarting.\n')
+        print('Trying again in 10 seconds.')
         time.sleep(10)
+        os.system('cls')
         main()
     else:
+        os.system('cls')
         checkip = ses.get('http://192.168.1.1/state/wan_state.asp').content
         soup = BeautifulSoup(checkip, 'html.parser')
         ip = soup.find(id = 'wan_ip').text
-        print('\nYour current ip is {}'.format(ip))
+        print('Your current ip is {}'.format(ip))
 
         of = open('ip/iprange.txt', encoding = 'utf-8').read()
         
         if ip == '0.0.0.0':
-            print('Invalid IP: {}'.format(ip))
-            time.sleep(2)
+            print('\n\nInvalid IP, restarting script in 10 seconds')
+            time.sleep(10)
+            os.system('cls')
             main()
         elif ip in of:
             print('The ip was found in the database, quitting the program.')
@@ -40,24 +46,27 @@ def main():
             print('ip not found in database\n\n')
             print('Restarting in 3 seconds.')
             time.sleep(3)
+            os.system('cls')
             reboot()
 
 def reboot():
     ses = requests.Session()
 
     try:
+        print('Connecting to ONU...')
         ses.post(url, data = login_data)
     except requests.exceptions.ConnectionError:
-        print('The router is still restarting\n')
-        time.sleep(1)
+        os.system('cls')
+        print('The router is still restarting.\n')
+        time.sleep(3)
         main()
     
     try:
         ses.get('http://192.168.1.1/goform/reboot')
     except requests.exceptions.ConnectionError:
-        print('\nThe router will now restart.')
+        print('\nRestarting ONU.')
         time.sleep(5)
+        os.system('cls')
         main()
-
 
 main()
