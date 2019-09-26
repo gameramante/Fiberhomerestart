d4f371b65d42431c10e704e0f39b51bcf6239897
gameramante <gamercaipira@gmail.com>
2019-09-26 12:43:11 -0300

    Added tries, refatored some parts of the code

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ login_data = {
             "Passwd": "***"
         }
 
-of = open('ip/iprange.txt', encoding = 'utf-8').read()
+of = open('ip/iprange.txt', encoding = 'utf-8').read() #open the ip.txt file
 
 url = 'http://192.168.1.1/goform/webLogin'
 
 ses = requests.Session()
 
-def main():
+def main(tries):
     try:
-        print('Trying to connect to ONU...')
-        time.sleep(1)
+        print('Connecting to ONU..')
+        time.sleep(0.5)
         ses.post(url, data = login_data) #login into the router
     except requests.exceptions.ConnectionError:
         os.system('cls')
-        print('\n\nONU is still restarting.\n')
-        print('Trying again in 15 seconds.')
-        time.sleep(15)
+        print('\nONU is still restarting.\n')
+        print('Trying again in 10 seconds.')
+        time.sleep(10)
         os.system('cls')
-        main()
+        main(tries = 0)
     else:
         os.system('cls')
         checkip = ses.get('http://192.168.1.1/state/wan_state.asp').content
@@ -35,10 +35,17 @@ def main():
         print('Your current ip is {}'.format(ip))
         
         if ip == '0.0.0.0':
-            print('\nInvalid IP, checking ip again in 15 seconds..')
-            time.sleep(15)
-            os.system('cls')
-            main()
+            print('\nInvalid IP, checking ip again in 3 seconds..')
+            tries += 1
+            print('\n\nCurrent try: {} remaining tries: {}'.format(tries, 15 - tries))
+            if tries >= 15:
+                print('Tries excedeed the limit, forcing restart\n Tries: {}\n'.format(tries))
+                tries = 0 #reset tries
+                reboot()
+            else:
+                time.sleep(3)
+                os.system('cls')
+                main(tries)
 
         elif ip in of:
             print('The ip was found in the database, quitting the program.')
@@ -59,14 +66,14 @@ def reboot():
         os.system('cls')
         print('The router is still restarting, trying again in 5 seconds\n')
         time.sleep(4)
-        main()
+        main(tries = 0)
     
     try:
         ses.get('http://192.168.1.1/goform/reboot')
     except requests.exceptions.ConnectionError:
-        print('\nONU is not restarting.')
+        print('\nONU is now restarting.')
         time.sleep(2)
         os.system('cls')
-        main()
+        main(tries = 0)
 
-main()
+main(tries = 0)
